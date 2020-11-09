26a00a16e11cf08a295ca303da0dd2cf73b3ba1c
Mailson <mayllsonribeiro6@gmail.com>
2020-11-09 17:48:09 -0300

    Adicionando navegação entre login e register

---
+++ app/src/main/java/com/mailsonribeiro/desafio/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.mailsonribeiro.desafio
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.android.material.button.MaterialButton
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(view)
+        view.findViewById<MaterialButton>(R.id.btnRegisterLogin).setOnClickListener{
+            navController.navigate(R.id.registerFragment)
+        }
+    }
+}
