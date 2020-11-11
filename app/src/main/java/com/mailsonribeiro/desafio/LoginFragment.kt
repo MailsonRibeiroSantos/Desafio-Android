0c2eddc8189d55eda8eb14c5ffcc901dd7a52268
Mailson <mayllsonribeiro6@gmail.com>
2020-11-11 18:52:44 -0300

    Adicionando telas de registro e login

--- app/src/main/java/com/mailsonribeiro/desafio/LoginFragment.kt
+++ app/src/main/java/com/mailsonribeiro/desafio/LoginFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 
 class LoginFragment : Fragment() {
-
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,9 +23,41 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val navController = Navigation.findNavController(view)
+        navController = Navigation.findNavController(view)
+        navegarRegister(view)
+        realizarLogin(view)
+
+
+    }
+    fun navegarRegister(view:View){
         view.findViewById<MaterialButton>(R.id.btnRegisterLogin).setOnClickListener{
             navController.navigate(R.id.registerFragment)
         }
+
+    }
+    fun realizarLogin(view: View){
+        view.findViewById<MaterialButton>(R.id.btnLoginLogin).setOnClickListener{
+           if(validarCampos(view)) {
+               navController.navigate(R.id.homeFragment)
+           }
+        }
+    }
+
+    fun validarCampos(view: View):Boolean{
+        var resultado = true
+
+        val editTextEmail = view.findViewById<TextInputEditText>(R.id.edtEmailLogin)
+        val editTextPassword = view.findViewById<TextInputEditText>(R.id.edtPasswordLogin)
+
+        if(editTextEmail.text.toString() == ""){
+            editTextEmail.error = "Empty field"
+            resultado = false
+        }
+
+        if(editTextPassword.text.toString() == ""){
+            editTextPassword.error = "Empty field"
+            resultado = false
+        }
+        return resultado
     }
 }
--- app/src/main/java/com/mailsonribeiro/desafio/RegisterFragment.kt
+++ app/src/main/java/com/mailsonribeiro/desafio/RegisterFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 
 class RegisterFragment : Fragment() {
@@ -27,8 +29,58 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val navController = Navigation.findNavController(view)
-        view.findViewById<MaterialButton>(R.id.btnRegisterResgiter).setOnClickListener{
-            navController.navigate(R.id.loginFragment)
+           view.findViewById<MaterialButton>(R.id.btnRegisterResgiter).setOnClickListener{
+               if(validarCampos(view)) {
+                   navController.navigate(R.id.loginFragment)
+               }
         }
+
+    }
+    fun validarCampos(view:View):Boolean{
+        var resultado = true
+        val editTextName = view.findViewById<TextInputEditText>(R.id.edtNameRegister)
+        val editTextEmail = view.findViewById<TextInputEditText>(R.id.edtEmailRegister)
+        val editTextPassword = view.findViewById<TextInputEditText>(R.id.edtPasswordRegister)
+        val editTextRepeatPassword = view.findViewById<TextInputEditText>(R.id.edtRepeatPasswordRegister)
+
+
+        if(editTextName.text.toString() == ""){
+            editTextName.error = "Empty field"
+            resultado = false
+        }
+
+        if(editTextEmail.text.toString() == ""){
+            editTextEmail.error = "Empty field"
+            resultado = false
+        }
+
+        if(editTextPassword.text.toString() == "") {
+            editTextPassword.error = "Empty field"
+            resultado = false
+        }
+
+        if(editTextRepeatPassword.text.toString() == "") {
+            editTextRepeatPassword.error = "Empty field"
+            resultado = false
+        }
+        resultado = validarSenha(view,editTextPassword.text.toString(),
+            editTextRepeatPassword.text.toString())
+
+        return resultado
+    }
+    fun validarSenha(view:View,senha:String, confirmarSenha:String):Boolean{
+        var resultado = true
+        if(senha != confirmarSenha){
+            Toast.makeText(view.context,
+                    "Password and confirm password must be the same",Toast.LENGTH_LONG).show()
+            resultado = false
+        }
+        if(senha.length<6){
+            Toast.makeText(view.context,
+                "The password must be at least six characters long",Toast.LENGTH_LONG).show()
+            resultado = false
+        }
+        return resultado
+
     }
 }
